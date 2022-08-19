72ea976f28b7bd5f31ee6a33f5eea306ca787841
Your Name <you@example.com>
2022-08-19 20:21:38 +0900

    Bringup CherishOS

--- cherish_vayu.mk
+++ cherish_vayu.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/cherish/config/common.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := cherish_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
