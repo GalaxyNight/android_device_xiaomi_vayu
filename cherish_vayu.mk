75a76f7edc04fb51de09878e939819a5addbf366
namenew___ <59400127+GalaxyNight@users.noreply.github.com>
2022-08-20 01:22:03 +0900

    Update cherish_vayu.mk

--- cherish_vayu.mk
+++ cherish_vayu.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/cherish/config/common.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 PRODUCT_NAME := cherish_vayu
 PRODUCT_DEVICE := vayu
