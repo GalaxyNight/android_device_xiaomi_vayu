f55a6138b3e97831bdb0475b546519861f93dece
Your Name <you@example.com>
2022-08-19 20:26:04 +0900

    Delete irrelevant items

--- cherish_vayu.mk
+++ cherish_vayu.mk
@@ -29,8 +29,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.3.0.SJUMIXM:user/release-keys
 
-# ArrowOS additions
-DEVICE_MAINTAINER := kubersharma001
-
 # CherishOS Vanilla build
 CHERISH_VANILLA := true
